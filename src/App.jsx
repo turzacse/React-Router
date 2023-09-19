c813bddbc5cc5474665248b8cb7fa380363d5104
turzacse <turzacse@gmail.com>
2023-09-19 21:12:55 +0600

    basic set for react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+    
+      <DaisyNav></DaisyNav>
+    </>
+  )
+}
+
+export default App
